7d79cc45abec5a55f14e3bdecd6fa1915a7ccda0
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2025-06-07 10:12:14 -0300

    feat: Create usuário

---
+++ create.php
@@ -0,0 +1,27 @@
+<?php 
+    require_once 'db.php';
+    require_once 'slack.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+        $stmt = $pdo -> prepare("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)");
+        $stmt -> execute([$nome, $email, $senha]);
+
+        sendSlackNotification("Novo usuário criado: $nome");
+
+        echo "Usuário criado com sucesso !";
+    }
+?>
+
+<form action="" method="POST">
+    Nome: <input type="text" name="nome" id=""> 
+    <br>
+    Email: <input type="email" name="email" id="">
+    <br>
+    Senha: <input type="password" name="senha" id="">
+    <br>
+    <button type="submit">Criar usuário</button>
+</form>
