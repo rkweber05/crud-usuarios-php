0652cf9f3528c06d0772963b2acbf933d81db6a4
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2025-06-10 08:43:13 -0300

    refact: Adjustment slack page

--- slack.php
+++ slack.php
@@ -1,6 +1,6 @@
 <?php 
     function sendSlackNotification($message) {
-        $webhook_url = 'https://hooks.slack.com/services/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'; // ver como que faz
+        $webhook_url = 'https://hooks.slack.com/services/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'; 
 
         $payload = json_encode(['text' => $message]);
 
@@ -9,7 +9,17 @@
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-        curl_exec($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        $error = curl_error($ch);
+
+        if ($error) {
+            error_log("Erro ao enviar para o Slack" . $error);
+        }
+
         curl_close($ch);
+
+        return $response;
     }
 ?>
