c4e8a5521221fdf64b0133e8457c38980123a65b
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2025-06-11 22:34:33 -0300

    feat: Adiciona frontend

--- create.php
+++ create.php
@@ -19,12 +19,34 @@
     }
 ?>
 
-<form action="" method="post">
-    Nome: <input type="text" name="nome" id=""> 
-    <br>
-    Email: <input type="email" name="email" id="">
-    <br>
-    Senha: <input type="password" name="senha" id="">
-    <br>
-    <button type="submit">Criar usuário</button>
-</form>
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastro usuário</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+  </head>
+  <body>
+    <form method="post">
+        <div class="mb-3">
+            <label class="form-label">Nome</label>
+            <input type="text" name="nome" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Email</label>
+            <input type="email" name="email" class="form-control" >
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Senha</label>
+            <input type="password" name="senha" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Criar usuário</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
