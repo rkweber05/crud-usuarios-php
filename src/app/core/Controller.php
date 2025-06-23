b40ddbfe50279db480734347e7ee0c86b125b421
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2025-06-23 08:02:29 -0300

    feat: Add basic controller

---
+++ src/app/core/Controller.php
@@ -0,0 +1,9 @@
+<?php
+    namespace App\Core;
+
+    class Controller {
+        public function view($view, $data = []) {
+            extract($data);
+            require_once __DIR__ . "/../view/{$view}.php";
+        }
+    }
